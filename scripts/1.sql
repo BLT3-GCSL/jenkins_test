f1922a8cf53eaee78762bcc1fd5a094d21ca0ac9
Austin Jr. <gcslsoftsupportblt3@globemw.net>
2024-10-03 16:13:27 +0200

    Removing Comments

--- scripts/1.sql
+++ scripts/1.sql
@@ -16,10 +16,8 @@ BEGIN CATCH
     DECLARE @ErrorMessage NVARCHAR(MAX)
     SET @ErrorMessage = ERROR_MESSAGE()
     
-    -- Log the error message
     INSERT INTO TEST_DB.dbo.ErrorLog (ErrorMessage) VALUES (@ErrorMessage)
 
-    -- Optionally, rethrow the error or handle it as needed
     RAISERROR(@ErrorMessage, 16, 1);
 END CATCH
 GO
